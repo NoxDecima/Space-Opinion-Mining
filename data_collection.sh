a4ed8ee42d4d57f1f5b97177e629d43670a09235
NoxDecima <sven@8x10.de>
2022-01-23 22:11:49 +0100

    Add all project files

---
+++ data_collection.sh
@@ -0,0 +1,3 @@
+twint -s "aerospace" --since 2019-10-01 --until 2019-12-31 --lang en --csv -o Pre-COVID.csv
+
+twint -s "aerospace" --since 2020-10-01 --until 2020-12-31 --lang en --csv -o In-COVID.csv
